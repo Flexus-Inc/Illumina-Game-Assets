fa5dc7f0f14b704115c58c8d5f913a27ed73bf81
Edrian Jose Ferrer <edrianjoseferrer@gmail.com>
2019-09-23 21:35:32 +0800

    Working scene transitions

--- _Scripts/__GLOBAL SCRIPTS/UIManager.cs
+++ _Scripts/__GLOBAL SCRIPTS/UIManager.cs
@@ -5,20 +5,4 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
     // Start is called before the first frame update
-    public static GameObject __GameUICanvas;
-    void Start() {
-        __GameUICanvas = GameObject.Find("__GameUICanvas");
-        __GameUICanvas.SetActive(false);
-    }
-    public static void EnableGUI() {
-        __GameUICanvas.SetActive(true);
-    }
-    public static void DisableGUI() {
-        __GameUICanvas.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
 }
