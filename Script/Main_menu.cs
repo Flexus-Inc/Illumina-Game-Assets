6673896d7b4ce47219ee8a10606efa842d4c9bf2
ejolalo <elaine_olalo09@yahoo.com>
2019-09-24 16:22:06 +0800

    Main menu(Settings)

---
+++ Script/Main_menu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour
+{
+    public AudioSource buttonclick;
+    public void Enclasp()
+    {
+        Debug.Log("Next scene");
+        buttonclick.Play();
+    }
+    public void Enlighten()
+    {
+        Debug.Log("Next scene");
+        buttonclick.Play();
+    }
+    public void Enchant()
+    {
+        buttonclick.Play();
+        SceneManager.LoadScene(sceneBuildIndex: 2);
+    }
+    public void editaccount(){
+        SceneManager.LoadScene(sceneBuildIndex: 3);
+    }
+
+    public GameObject Panel;
+
+    public void OpenPanel(){
+        if(Panel != null){
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+            Animator animator = Panel.GetComponent<Animator>();
+            if(animator !=null){
+                bool isOpen = animator.GetBool("open");
+
+                animator.SetBool("open", !isOpen);
+            }
+        }
+    }
+}
---
+++ Script/SceneTransitions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public int index;
+    public Image black;
+    public Animator animator;
+
+    public void buttonclicked(){
+        StartCoroutine(fade());
+    }
+
+    IEnumerator fade(){
+        animator.SetBool("fade",true);
+        yield return new WaitUntil(()=>black.color.a==1);
+        SceneManager.LoadScene(index);
+    }
+}
