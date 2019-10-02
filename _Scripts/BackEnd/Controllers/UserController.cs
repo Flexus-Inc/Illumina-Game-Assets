c9df2c22bc8dc81d27c04d48dbf97a8151b7245a
Edrian Jose Ferrer <edrianjoseferrer@gmail.com>
2019-10-02 10:07:59 +0800

    EmailVerification system created

--- _Scripts/BackEnd/Controllers/UserController.cs
+++ _Scripts/BackEnd/Controllers/UserController.cs
@@ -8,6 +8,7 @@ using static Illumina.Networking.Request;
 namespace Illumina.Controller {
 
     public class UserController : Controller {
+        public static string password;
 
         public static void UserExists(string identifier, string value, RequestSuccessEventHandler e, RequestFailedEventHandler f) {
             var tokenQuery = IlluminaHash.GetUniqueDateTimeHash();
@@ -21,9 +22,21 @@ namespace Illumina.Controller {
             IlluminaWebRequest.Get(userExistRequest);
         }
 
-        public static void Signup(User user) {
+        public static void VerifyEmail(User user) {
             user.password = IlluminaHash.GetHash(user.password);
-            //user.username = IlluminaCipher.Encipher(user.username);
+            Request verifyRequest = new Request {
+                uri = "/user/verifyemail/" + IlluminaHash.GetUniqueDateTimeHash(),
+                body = user
+            };
+
+            verifyRequest.RequestSuccessEvents += OnVerifyRequestSuccess;
+            verifyRequest.RequestFailedEvents += OnVerifyRequestFailed;
+            Store<User>(verifyRequest);
+        }
+
+        public static void Signup(User user) {
+            password = user.password;
+            user.password = IlluminaHash.GetHash(user.password);
             Request signupRequest = new Request {
                 uri = "/user",
                 body = user
@@ -33,13 +46,26 @@ namespace Illumina.Controller {
             Store<User>(signupRequest);
         }
 
+        public static void OnVerifyRequestSuccess(object source) {
+            var user = (User) source;
+            Debug.Log("Code is sent to the email : " + user.email);
+        }
+
+        public static void OnVerifyRequestFailed(Exception err) {
+            Debug.Log(err);
+            UIManager.Alert(err.Message);
+        }
         public static void OnSignUpRequestSuccess(object source) {
             var user = (User) source;
-            Debug.Log(user.GetServerMessage());
+            user.logged_in = true;
+            user.password = password;
+            Login(user);
+            user.password = null;
         }
 
         public static void OnSignUpRequestFailed(Exception err) {
             Debug.Log(err);
+            UIManager.Alert(err.Message);
         }
         public static void Login(User user) {
             user.password = IlluminaHash.GetHash(user.password);
@@ -61,6 +87,29 @@ namespace Illumina.Controller {
         }
 
         public static void OnLoginRequestFailed(Exception err) {
+            Debug.Log(err);
+            UIManager.Alert(err.Message);
+        }
+
+        public static void Logout(User user) {
+            Request logoutRequest = new Request {
+                uri = "/user/logout",
+                body = user
+            };
+
+            logoutRequest.RequestSuccessEvents += OnLogoutRequestSuccess;
+            logoutRequest.RequestFailedEvents += OnLogoutRequestSuccess;
+            Update<User>(logoutRequest);
+        }
+
+        public static void OnLogoutRequestSuccess(object source) {
+            GameData.User = (User) source;
+            Debug.Log(GameData.User.GetServerMessage());
+            UIManager.HideLoading();
+            ScenesManager.GoToScene(3);
+        }
+
+        public static void OnLogoutRequestFailed(Exception err) {
             Debug.Log(err);
             UIManager.Alert(err.Message);
         }
