994ba6a94a6426c2fa9d3b833025ae060f6cde4f
ejolalo <elaine_olalo09@yahoo.com>
2019-10-02 10:21:47 +0800

    UI Interaction

---
+++ _Scripts/FrontEnd/08 Game/PositionAnimator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionAnimator : MonoBehaviour
+{
+    public Sprite[] baybayinImg;
+    public Image img;
+    private Vector3 currentPos, targetPos;
+    [SerializeField] private bool repeatable = true;
+    [SerializeField] private float LimitXAxis = 1920f;
+    [SerializeField] private float LimitYAxis = 1080f;
+    private float speed; 
+    private float duration;  
+    float x, y;
+    int randomImg;
+    IEnumerator Start()
+    {
+        randomImg = Random.Range(0, baybayinImg.Length);
+        img.sprite = baybayinImg[randomImg];
+        Debug.Log(img.sprite);
+        speed = Random.Range(2f, 3f);
+        duration = Random.Range(10f, 25f);
+        currentPos = transform.localPosition;
+        img.canvasRenderer.SetAlpha(Random.Range(3f, 5f));
+        while (repeatable)
+        {
+            
+            x = Random.Range(-(LimitXAxis / 2f), (LimitXAxis / 2f));
+            y = Random.Range(-(LimitYAxis / 2f), (LimitYAxis / 2f));
+            targetPos = new Vector3(x, y);
+            
+            yield return Position(currentPos, targetPos, duration);
+            //yield return Position(targetPos, currentPos, duration);
+        }
+    }
+
+    IEnumerator Position(Vector3 currPos, Vector3 updatePos, float time)
+    {
+        float _start = 0.0f;
+        float rate = (1.0f / time) * speed;
+        
+        while (_start < 1f)
+        {
+            transform.localPosition = Vector3.Slerp(currPos, updatePos, _start);
+            if (_start <= 0.6f)
+                img.CrossFadeAlpha(1, 0.6f, false);
+            else
+                img.CrossFadeAlpha(0, 0.40f, false);
+            _start += Time.deltaTime * rate;
+            yield return null;
+        }
+    }
+}
