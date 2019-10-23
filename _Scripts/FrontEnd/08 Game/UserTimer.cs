9814f4d20f6f3fdf6cee9b9748f379b9aefc1fe2
ejolalo <elaine_olalo09@yahoo.com>
2019-10-23 14:34:39 +0800

    User Timer

--- _Scripts/FrontEnd/08 Game/UserTimer.cs
+++ _Scripts/FrontEnd/08 Game/UserTimer.cs
@@ -5,50 +5,26 @@ using UnityEngine.UI;
 
 public class UserTimer : MonoBehaviour
 {
- public Image loading;
- //public Text timeText;
- public int minutes;
- public int sec;
- int totalSeconds = 0;
- int TOTAL_SECONDS = 0;
- float fillamount;
+    public Image loading;
+    public float sec;
 
- void Start ()
- {
-  //timeText.text = minutes + " : " + sec;
-  if (minutes > 0)
-   totalSeconds += minutes * 60;
-  if (sec > 0)
-   totalSeconds += sec;
-  TOTAL_SECONDS = totalSeconds;
-  StartCoroutine (second ());
- }
+    void Start ()
+        {
+            StartCoroutine (StartTimer());
+        }
 
- void Update ()
- {
-  if (sec == 0 && minutes == 0) {
-   //timeText.text = "Time's Up!";
-   StopCoroutine (second ());
-  }
- }
- IEnumerator second()
- {
-  yield return new WaitForSeconds (1f);
-  if(sec > 0)
-  sec--;
-  if (sec == 0 && minutes != 0) {
-   sec = 60;
-   minutes--;
-  } 
-  //timeText.text = minutes + " : " + sec;
-  fillLoading ();
-  StartCoroutine (second ());
- }
+    IEnumerator StartTimer(){
+        float timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / sec;
 
- void fillLoading()
- {
-  totalSeconds--;
-  float fill = (float)totalSeconds/TOTAL_SECONDS;
-  loading.fillAmount = fill;
- }
+        while (true) {
+            timeSinceStarted = Time.time - timeStartedLerping;
+            percentageComplete = timeSinceStarted / sec;
+            loading.fillAmount = 1 - percentageComplete;
+            if (percentageComplete >= 1) break;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
 }
